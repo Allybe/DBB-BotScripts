d062e80a39d8603c784878b5085026281a92851a
Allybe <creeperplay1212@gmail.com>
2023-07-19 13:33:58 -0500

    First working version!

--- src/interfaces/AllyClient.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from "discord.js";
-import { Collection } from "@discordjs/collection";
-import { Command } from "./Commands";
-
-export class AllyClient extends Client {
-    public commands: Collection<String, Command> = new Collection;
-}
