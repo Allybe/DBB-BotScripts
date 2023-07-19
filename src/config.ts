485ca6590d95cdfa6857b579a95bdf84983c9df9
Allybe <creeperplay1212@gmail.com>
2023-07-19 13:24:41 -0500

    First working version!

--- src/config.ts
+++ src/config.ts
@@ -16,27 +16,13 @@ interface Config {
     guildID: string;
 }
 
-/*export const config: Config = {
+export const config: Config = {
     token: "token",
     prefix: "prefix",
     applicationID: "applicationID",
     clientID: "clientID",
     botName: "botName",
     botAuthor: "botAuthor",
-
-    status: "online",
-    intents: ["Guilds"],
-    partials: [],
-    RESTVersion: "9",
-}*/
-
-export const config: Config = {
-    token: "MTEzMTIzNzM2ODA2Njc0MDMyNA.GmhXNp.QDL3j9SdZGRP98WIPzz0JlBqa6RC0ZXJeJzplE",
-    prefix: "!",
-    applicationID: "1131237368066740324",
-    clientID: "1131237368066740324",
-    botName: "TestBot",
-    botAuthor: "Allison",
 
     status: "online",
     intents: ["Guilds"],
